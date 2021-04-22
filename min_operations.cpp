bf2d15d3f6ceab6b07381085674d0271a618f9b4
MayureeS <mayureesawal123@gmail.com>
2021-04-22 16:11:34 +0530

    Added code

--- min_operations.cpp
+++ min_operations.cpp
@@ -1,22 +1,23 @@
 /*
- * Problem: The input is n x n square matrix. The output is minimum number of operations required to convert input
- *          matrix into the matrix in which sum of elements of each row & column is equal. One operation means
- *          incrementing any value of cell of matrix by 1. Also, the converted matrix is displayed on screen.
- * For Ex: I/P = 2
- *1 2
- *3 4
- *
- *O/P : 4
- *4 3
- *3 4
- *In above example, input matrix is converted so that sum of each row and column of output matrix is 7. The minimum number of
- *operations required to do so are 4.
- *
- *Solution: In find_min_steps function, first the sum of individual row and column is stored in 2 arrays, one for
- *             storing sum of rows & other for sum of columns. The maximum of both the arrays is calculated(i.e
- *             maximum of sum of cells in individual rows & columns.) By iterating over the matrix and performing
- *             addition wherever required the sum of all individual rows & columns is made equal to maximum sum.
+Problem: The input is n x n square matrix. The output is minimum number of operations required to convert input
+         matrix into the matrix in which sum of elements of each row & column is equal. One operation means
+         incrementing any value of cell of matrix by 1. Also, the converted matrix is displayed on screen.
+For Ex: I/P = 2
+1 2
+3 4
+
+O/P : 4
+4 3
+3 4
+In above example, input matrix is converted so that sum of each row and column of output matrix is 7. The minimum number of
+operations required to do so are 4.
+
+Solution: In find_min_steps function, first the sum of individual row and column is stored in 2 arrays, one for
+          storing sum of rows & other for sum of columns. The maximum of both the arrays is calculated(i.e
+          maximum of sum of cells in individual rows & columns.) By iterating over the matrix and performing
+          addition wherever required the sum of all individual rows & columns is made equal to maximum sum.
 */
+
 #include<iostream>
 using namespace std ;
 
@@ -27,8 +28,8 @@ int find_min_steps(int fn , int **finput_matrix)
     int i = 0, j = 0, maxSum = 0, operations = 0, diff = 0 ;
 
     /*
-     * Assigning memory to 2 sum arrays. rowSum stores the sum of cells of individual rows & columnSum stores the sum
-    *of cells of individual columns.
+    Assigning memory to 2 sum arrays. rowSum stores the sum of cells of individual rows & columnSum stores the sum
+    of cells of individual columns.
     */
     rowSum = new int[fn] ;
     columnSum = new int[fn] ;
@@ -54,13 +55,13 @@ int find_min_steps(int fn , int **finput_matrix)
     for(i=0, j=0 ; i<fn && j<fn ;)
     {
         /*
-         * We calculate the difference between maximum sum and sum of elements of ith row and jth column respectively.This
-         * minimum difference is added to the element [i][j]. Minimum among the both differences is considered so as to avoid
-         * increasing any row or column sum beyond the maximum sum.
-         * As the value of [i][j] th element is increased by diff, this diff has to be added in sum arrays rowSum[i] & columnSum[j].
-         * According to the problem statement, one operation means addition by one, hence the difference has to be added in number of
-         * operations as well.
-         */
+        We calculate the difference between maximum sum and sum of elements of ith row and jth column respectively.This
+        minimum difference is added to the element [i][j]. Minimum among the both differences is considered so as to avoid
+        increasing any row or column sum beyond the maximum sum.
+        As the value of [i][j] th element is increased by diff, this diff has to be added in sum arrays rowSum[i] & columnSum[j].
+        According to the problem statement, one operation means addition by one, hence the difference has to be added in number of
+        operations as well.
+        */
         diff = min(maxSum - rowSum[i] , maxSum - columnSum[j]) ;
         finput_matrix[i][j] = finput_matrix[i][j] + diff ;
         rowSum[i] = rowSum[i] + diff ;
@@ -78,6 +79,11 @@ int find_min_steps(int fn , int **finput_matrix)
             j = j + 1 ;
         }
     }
+
+    //Deallocate memory assigned to rowSum & columnSum array.
+    delete[] rowSum ;
+    delete[] columnSum ;
+
     return operations ;
 }
 
@@ -133,25 +139,26 @@ int main(void)
 }
 
 /*
- * Sample I/O :
- * I/O:
- * 3
- * 1 2 3 4 5 6 7 8 9
- *
- * O/P:
- * 13 8  3
- * 4  8  12
- * 7  8  9
- *
- * Explanation: Here, matrix size is 3 i.e 3x3 matrix.n next line each element of matrix is entered in row wise manner
- * separated by space. So input matrix is :   1 2 3
- *                                            4 5 6
- *                                            7 8 9
- *Maximum sum in this matrix is of last row i.e 7+8+9 = 24. So sum of each row and column is made equal to 24 by
- *performing addition.
- *
- *
- *Time Complexity: O(n^2)
- *Space Complexity: O(n)
- */
+Sample I/O :
+I/O:
+3
+1 2 3 4 5 6 7 8 9
+
+O/P:
+27
+13 8  3
+4  8  12
+7  8  9
+
+Explanation: Here, matrix size is 3 i.e 3x3 matrix.n next line each element of matrix is entered in row wise manner
+             separated by space. So input matrix is :   1 2 3
+                                                        4 5 6
+                                                        7 8 9
+Maximum sum in this matrix is of last row i.e 7+8+9 = 24. So sum of each row and column is made equal to 24 by
+performing addition. The minimum number of operations needed to convert input matrix is 27.
+
+
+Time Complexity: O(n^2)
+ Space Complexity: O(n)
+*/
 
